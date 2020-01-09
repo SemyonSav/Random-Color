c674b64691cf6edf4a933eb946c55eed32a7f632
SemyonSav <59660695+SemyonSav@users.noreply.github.com>
2020-01-09 13:44:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
-import sys
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QLabel, QGridLayout
+from PyQt5.QtGui import QPainter, QPixmap, QPen, QColor
+from PyQt5.QtCore import Qt
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
-from PyQt5.QtGui import QPainter, QColor, QPen
-import random
+from random import randint
 
 
-class MyWidget(QMainWindow):
+class Test(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
-        self.pushButton.clicked.connect(self.paintEvent)
+        uic.loadUi('untitled.ui', self)
 
-    def paintEvent(self, event):
-        qp = QPainter()
-        qp.begin(self)
-        self.drawFlag(qp)
-        qp.end()
+        self.pushButton.setFixedSize(100, 100)
+        self.pushButton.clicked.connect(self.circle)
 
-    def drawFlag(self, qp):
-        qp.setBrush(QColor('Yellow'))
-        for i in range(random.randint(0, 60)):
-            qp.drawEllipse(random.randint, random.randint, 80, 80)
+        self.label = QLabel()
+        canvas = QPixmap(600, 600)
+        self.label.setPixmap(canvas)
+
+        layout = QGridLayout(self.centralwidget)
+        layout.addWidget(self.pushButton, 0, 0, alignment=Qt.AlignCenter)
+        layout.addWidget(self.label, 1, 0)
+
+    def circle(self):
+        x, y = [randint(10, 500) for i in range(2)]
+        w, h = [randint(10, 100) for i in range(2)]
+        # создаем экземпляр QPainter, передавая холст (self.label.pixmap())
+        painter = QPainter(self.label.pixmap())
+        pen = QPen()
+        pen.setWidth(3)
+        pen.setColor(QColor(255, 255, 0))
+        painter.setPen(pen)
+        painter.drawEllipse(x, y, w, h)
+        painter.end()
+        self.update()
 
 
-app = QApplication(sys.argv)
-ex = MyWidget()
-ex.show()
-sys.exit(app.exec_())
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+    w = Test()
+    w.show()
+    sys.exit(app.exec_())
